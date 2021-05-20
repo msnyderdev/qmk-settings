c5ee330ee9bf88bdfa62c0f56f5befc8be0fb1ea
Mike Snyder <msnyder.pro@gmail.com>
2021-05-20 13:04:41 -0400

    Need two spacebars

--- spaceman/pancake/keymaps/mike/keymap.c
+++ spaceman/pancake/keymaps/mike/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     HOME_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,       KC_O,      KC_P,     KC_BSPC,
     KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,     KC_K,       KC_L,      KC_SCLN,  KC_QUOT,
     HOME_TAB, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM,    KC_DOT,    KC_SLSH,  KC_BSLS,
-    KC_LALT,  KC_LGUI, FN,      LOWER,   RAISE,   KC_SPC,  KC_ENT,  KC_DEL,   KC_LBRC,    KC_RBRC,   KC_MINUS, KC_EQUAL
+    KC_LALT,  KC_LGUI, FN,      LOWER,   RAISE,   KC_SPC,  KC_SPC,  KC_ENT,   KC_LBRC,    KC_RBRC,   KC_MINUS, KC_EQUAL
 ),
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_BSPC,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,
     _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_UNDS,  KC_PLUS
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_DEL,
     _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______
